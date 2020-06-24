d0b88ee62fa0163cd085287c6b630519f01868eb
Natanael Saymon <nsaymonsouza@gmail.com>
2020-06-24 14:55:35 -0300

    Done: Menu mobile concluido

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
 // MENU MOBILE
 const menuMobile = document.querySelector('#icon')
 const nav = document.querySelector('#nav-list')
-const i = document.querySelector('#hamburguer')
-const fechar = document.querySelector('#block')
+const bars = document.querySelector('#bars')
+const times = document.querySelector('#times')
 
 menuMobile.addEventListener("click", () => {
   nav.classList.toggle('show')
 })
 
-i.addEventListener("click", () =>{
-  fechar.style.display = 'block'
-  i.style.display = 'none'
+bars.addEventListener("click", () =>{
+  times.style.display = 'block'
+  bars.style.display = 'none'
 })
 
-fechar.addEventListener("click", () => {
-  i.style.display = 'block'
-  fechar.style.display = 'none'
+times.addEventListener("click", () => {
+  bars.style.display = 'block'
+  times.style.display = 'none'
 })
 
 const links = document.querySelectorAll('#nav-list li a')
@@ -23,5 +23,7 @@ const links = document.querySelectorAll('#nav-list li a')
 for(link of links){
   link.addEventListener("click", () =>{
     nav.classList.remove('show')
+    times.style.display = 'none'
+    bars.style.display = 'block'
   })
 }
